fcbe8a9c0c51fe65fa56823d285fed628fac1a05
guillergm <104893103+guillergm@users.noreply.github.com>
2022-06-09 23:09:19 +0200

    Add files via upload

---
+++ EJERCICIOS BUCLES/pruebas_asteriscos.js
@@ -0,0 +1,28 @@
+//funcion para crear filas con "k" asteriscos
+function fila(k) { // k es el numero de asteriscos que se van a imprimir
+  let n = 0;// contador
+  let linea = " ";//fila de asteriscos
+  while (n < k) {
+    linea = linea + "*";
+    n++;
+  }
+  console.log(linea);
+}
+function piramide(k) {
+  let n = 0;
+  while (n < k) {
+    n++;
+    fila(n);
+  }
+}
+function piramideInvertida(k) {
+  while (k > 0) {
+    fila(k);
+    k--;
+  }
+}
+n=0;
+while(n<25){
+  n++;
+  piramide(n);
+}
