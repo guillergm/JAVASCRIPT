f147a9e19f0581636dfdc201d3a6fa665ca47778
guillergm <104893103+guillergm@users.noreply.github.com>
2022-07-04 23:35:52 +0200

    Add files via upload

---
+++ ejercicios/if_elseif_else.js
@@ -0,0 +1,17 @@
+nombre = "guille"
+
+if (nombre = "guille") {
+    alert("ese es tu nombre");
+}
+
+else if (nombre = "ruiz") {
+    alert("ese es tu primer apellido");
+}
+
+else if(nombre = "gimenez") {
+    alert("ese es tu segundo apellido");
+}
+
+else {
+    alert("ese NO es tu nombre");
+}
